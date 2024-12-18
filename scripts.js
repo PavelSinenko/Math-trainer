e3ee2d91e1481c81a4d7e3e3b0db4a0e2d5c8953
PavelSinenko <sinenko.pavel72@gmail.com>
2024-12-18 21:44:18 +0300

    just an examples

--- scripts.js
+++ scripts.js
@@ -1,4 +1,50 @@
-const math = () => {
-    console.log('Hello world!');
-};
-math();
+const guessNumber = (string) => {
+    let randomNumber = Math.floor(Math.random() * 5);
+    console.log(randomNumber);
+    if (number === randomNumber) {
+        console.log('Yeeeah!');
+    } else {
+        console.log('Nooo!')
+    }
+
+}
+guessNumber()
+
+
+// const answer = (number) => {
+// let sum = 2 + 2;
+// if (number === sum) {
+//     console.log('Right');
+// } else {
+//     console.log ('Wrong!');
+// }
+// }
+
+// answer(4)
+
+// function getRandomInt() {
+//     let first = Math.floor(Math.random() * 9);
+//     let second = Math.floor(Math.random() * 9);
+//     let third = Math.floor(Math.random() * 10); 
+//     let signs = '';
+//     if (third <= 5){
+//         signs = '-';
+//     } else {
+//         signs = '+'
+//     }
+//     console.log(`${first} ${signs} ${second} = ?`)
+//   }
+  
+//   getRandomInt()
+
+
+
+// const test = () => {
+//     document.getElementById('answer').textContent='64 + 44 = ?'
+// }
+// document.addEventListener('DOMContentLoaded',function(){test()})
+// document.addEventListener('keypress', function(ev){
+//     if( === 'Enter'){
+//         document.getElementById('answer').textContent='33 - 22 = ?';
+//     }
+// })
