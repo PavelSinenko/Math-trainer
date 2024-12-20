94bda247a70583f0bc3eeb97715653a30b1dd003
PavelSinenko <sinenko.pavel72@gmail.com>
2024-12-20 14:34:17 +0300

    complete tasks 1.3

--- scripts.js
+++ scripts.js
@@ -1,46 +1,29 @@
-// Задание 1.2
-// No.1 Дано число. Выведите в консоль первую цифру этого числа.
-const figure = (number) => {
-    console.log(`First number: ${number.toString()[0]}`);
-}
-figure(384);
-
-//No.2 Дано число. Выведите в консоль последнюю цифру этого числа.
-// const figure = (number) => {
-//     console.log(`Last number: ${number % 10}`);
-//     }
-//     figure(4643);
-
-// //No.3 Дано число. Выведите в консоль сумму первой и последней цифры этого числа.
-// const sumOfNumbers = (number) => {
-//     let figure = number.toString();
-//     let first = parseInt(figure[0]); 
-//     let last = parseInt(figure[figure.length - 1]);
-
-// console.log(`Our sum: ${first + last}`); 
-// }
-// sumOfNumbers(57899);
-
-//No.4 Дано число. Выведите количество цифр в этом числе.
-// const count = (number) => {
-//    let figure = number.toString().length;
-//    console.log(`Count of numbers: ${figure}`);
-// }
-// count(567386789);
-
-//No.5 Даны два числа. Проверьте, что первые цифры этих чисел совпадают.
-// const sameNumbers = (number1, number2) => {
-//     console.log(number1, number2);
-//     let first = number1.toString();
-//     let second = number2.toString();
-//     if(first[0] === second[0]) {
-//         console.log('First numbers are the same');
+// Задание 1.3
+// No.1 Дана строка. Если в этой строке более одного символа, выведите в консоль предпоследний символ этой строки.
+// const predLast = (string) => {
+//     console.log(string);
+//     if (string.length > 1) {
+//         console.log(`Предпоследний символ: ${string[string.length - 2]}`);
 //     } else {
-//         console.log('No, first numbers are differnet')
+//         console.log('Только один символ');
 //     }
-
 // }
-// sameNumbers(544, 58);
+// predLast('Hello world!');
+
+//No.2 Даны два целых числа. Проверьте, что первое число без остатка делится на второе.
+const justNumber = (number1, number2) => {
+      if (number1 % number2 === 0) {
+        console.log('Yeeeah!');
+      } else {
+        console.log('Oh, nooo!')
+      }
+}
+justNumber(33, 3);
+
+
+
+
+
 
 
 // Предыдущие функции
