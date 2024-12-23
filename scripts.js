4ab0ce6478188422fe584072d7ce69268e2a09fd
PavelSinenko <sinenko.pavel72@gmail.com>
2024-12-23 12:34:26 +0300

    complete tasks 1.9

--- scripts.js
+++ scripts.js
@@ -1,29 +1,22 @@
-// Задание 1.8
-//No.1 Заполните массив целыми числами от 1 до 10.
-// const mass = [];
-// for (let i = 1; i <= 10; i++) {
-//     mass.push(i);
-// }
-// console.log(mass); // Выводит массив [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ]
+// Задание 1.9
+//No.1 Дан массив со строками. Оставьте в этом массиве только те строки, которые начинаются на http://.
+// const mass = ['http://facebook', 'http://instagram', 'youtube'];
+// let res = mass.filter(elem => {
+//         return elem.startsWith('http://');
+//     });
+// console.log(res); // Выводит [ 'http://facebook', 'http://instagram' ]
 
-//No.2 Заполните массив четными числами из промежутка от 1 до 100.
-// const mass = [];
-// for (let i = 1; i <= 100; i++) {
-//    if(i % 2 === 0) {
-//     mass.push(i);
-//    }
-// }
-// console.log(mass); // Выводит большой массив из четных чисел
+//No.2 Дан массив со строками. Оставьте в этом массиве только те строки, которые заканчиваются на .html.
+// const mass = ['http://facebook', 'project.html', 'youtube', 'window.html'];
+// let res = mass.filter(elem => {
+//         return elem.endsWith('.html');
+//     });
+// console.log(res); // Выводит [ 'project.html', 'window.html' ]
 
-//No.3 Дан массив с дробями: [1.456, 2.125, 3.32, 4.1, 5.34] Округлите эти дроби до одного знака в дробной части.
-const mass = [1.456, 2.125, 3.32, 4.1, 5.34];
-const otherMassive = mass.reduce((res, num) => {
-    res.push(Math.round(num * 10) / 10);
-    return res;
-    }, []);
-
-console.log(otherMassive); // Выводит: [ 1.5, 2.1, 3.3, 4.1, 5.3 ]
-
+//No.3 Дан массив с числами. Увеличьте каждое число из массива на 10 процентов.
+const mass = [10, 20, 30, 100]
+let answer = mass.map((x) => x + (x * 0.1));
+console.log(answer); // Ответ [ 11, 22, 33, 110 ]
 
 
 // Предыдущие функции
